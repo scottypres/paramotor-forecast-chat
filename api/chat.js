ab8984965cd73179e68e0dad806f606bd9726782
scottypres <50996584+scottypres@users.noreply.github.com>
2025-03-30 05:35:47 -0400

    commonJS

--- api/chat.js
+++ api/chat.js
@@ -1,6 +1,6 @@
-import OpenAI from 'openai';
+const OpenAI = require('openai');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
